995127927ffb300e6be1c09a81146ca4ea094e1e
rizwanakhtarr <you@example.com>
2018-06-06 04:50:43 +0100

    made instructions for game which when user clicks on button will show and hide them accordingly

--- js/main.js
+++ js/main.js
@@ -1,18 +1,35 @@
 $(document).ready(function(){
 
-//This will move padels for the red and blue  up and down when user clicks on up and down on keyboard
+  $("#hide_instructions").click(function(){ // This will hide my instructions when click on button
+    $("p").hide();
+  });
+
+  $("#show_instructions").click(function(){ // this will show my instructions when user clicks on the button
+    $("p").show();
+  });
+
+
+
+
+  setInterval(function(){
+
+
+            console.log($("#ball").collision("#bat_1"));
+  }, 10);
+
+  //This will move padels for the red and blue  up and down when user clicks on up and down on keyboard
 
   $(window).keydown(function(e){
     // alert(e.keyCode);
     var position = $("#bat_1").position();
     switch (e.keyCode) {
       case 38:
-        $("#bat_1").css('top', position.top - 15 + 'px');
-        break;
+      $("#bat_1").css('top', position.top - 15 + 'px');
+      break;
 
       case 40:
-        $("#bat_1").css('top', position.top + 15 + 'px');
-        break;
+      $("#bat_1").css('top', position.top + 15 + 'px');
+      break;
 
     }
   });
@@ -22,12 +39,12 @@ $(document).ready(function(){
     var position = $("#bat_2").position();
     switch (e.keyCode) {
       case 87:
-        $("#bat_2").css('top', position.top - 15 + 'px');
-        break;
+      $("#bat_2").css('top', position.top - 15 + 'px');
+      break;
 
       case 83:
-        $("#bat_2").css('top', position.top + 15 + 'px');
-        break;
+      $("#bat_2").css('top', position.top + 15 + 'px');
+      break;
 
     }
   });
@@ -39,16 +56,18 @@ $(document).ready(function(){
   var interval;
   var timerRunning = false;
   var $container = $('#container');
-  console.log($container);
+
   var $ball = $('#ball');
   var $redBat = $('#bat_1');
   var $blueBat = $('#bat_2');
   var posX = 250;
   var posY = 175;
 
-
   var directionX = '+';
   var directionY = '+';
+
+  var ballObj = {};
+  var redObj = {};
 
   // set the intervals for the button
   $('#btn').click(function(){
@@ -58,30 +77,47 @@ $(document).ready(function(){
     } else {
       interval = setInterval(function() {
 
+          console.log($("#ball").collision("#bat_1"));
+
+        // Check the balls position
+        var ballLeft = $ball.position().left;
+        var ballTop = $ball.position().top;
+        var ballRight = ballLeft + 20;
+        var ballBottom = ballTop + 20;
+
+
+
+
+
         // Blue bat
         var blueLeft = $blueBat.position().left;
         var blueTop = $blueBat.position().top;
         var blueRight = blueLeft + $blueBat.width();
         var blueBottom = blueTop + $blueBat.height();
 
-        // Check the balls position
-        var ballLeft = $ball.position().left;
-        var ballTop = $ball.position().top;
-        var ballRight = ballLeft + $ball.width();
-        var ballBottom = ballTop + $ball.height();
-
         // Red bat
         var redLeft = $redBat.position().left;
         var redTop = $redBat.position().top;
-        var redRight = redLeft + $redBat.width();
-        var redBottom = redTop + $redBat.height();
+        var redRight = redLeft + 25;
+        var redBottom = redTop + 150;
+
+
+
 
         //container
         var containerLeft = $container.position().left;
         var containerTop = $container.position().top;
-        var containerRight = containerLeft + $container.innerWidth();
-        var containerBottom = containerTop + $container.innerHeight();
-        console.log(containerTop);
+        var containerRight = containerLeft + 25;
+        var containerBottom = containerTop + 150;
+
+
+        // ====== Change ball direction when paddle hit ========
+
+
+
+        // ====== Change ball direction when paddle hit ========
+
+        // ====== Move the Ball between the board boundaries ========
         // Move ball along X-Axis
         if (directionX === '+') {
           $ball.css({
@@ -109,28 +145,28 @@ $(document).ready(function(){
           posY--;
         }
 
-        if (ballRight > blueLeft) {
-          directionX = '-';
-        }
-        if (ballLeft < redRight) {
-          directionX = '+';
-        }
-
-        //up and down
-        if (ballRight > containerRight) {
-          directionX = '-';
-        }
         if (ballLeft < containerLeft) {
           directionX = '+';
         }
-        if (ballBottom > 350) {
-          directionY = '-';
-        }
-        if (ballTop < 0) {
-          directionY = '+';
+
+        if (ballRight > containerRight) {
+          directionX = '-';
         }
 
-      }, 1);
+        if (ballTop < containerTop) {
+          directionY = '-';
+
+        }
+
+        if (ballBottom > containerBottom) {
+          directionY = '+'
+        }
+
+        // ============================================
+
+
+
+      }, 5);
       timerRunning = !timerRunning;
     }
   });
